f4116298f36af4d628e92464e818499234e0fe42
blue-jam <blue-jam@users.noreply.github.com>
2019-07-12 00:25:37 +0900

    Update exercise4.1

--- bluejam/chap4_exercise1.lean
+++ bluejam/chap4_exercise1.lean
@@ -21,5 +21,26 @@ example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
             have q y, from h.right y,
             show p y ∧ q y, from and.intro ‹ p y › ‹ q y › 
         )
-example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := sorry
-example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
+
+example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) :=
+    assume h: (∀ x, p x → q x),
+    assume hp: (∀ x, p x),
+    assume y: α,
+    show q y, from (h y) (hp y)
+
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x :=
+    assume h: (∀ x, p x) ∨ (∀ x, q x),
+    or.elim
+        h
+        (
+            assume hl: ∀ x, p x,
+            assume y: α,
+            have p y, from hl y,
+            or.intro_left (q y) this
+        )
+        (
+            assume hr: ∀ x, q x,
+            assume y: α,
+            have q y, from hr y,
+            or.intro_right (p y) this
+        )

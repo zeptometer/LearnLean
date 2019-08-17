f5985c3eb66d7e3436a1134cc846e8f7640802db
blue-jam <blue-jam@users.noreply.github.com>
2019-08-17 21:48:43 +0900

    Update chap5_exercise5.2.lean

--- bluejam/chap5_exercise5.2.lean
+++ bluejam/chap5_exercise5.2.lean
@@ -1,3 +1,18 @@
 example (p q r : Prop) (hp : p) :
+    (p ∨ q ∨ r) ∧ (q ∨ p ∨ r) ∧ (q ∨ r ∨ p) :=
+begin
+  split,
+    left,
+    assumption,
+  split,
+    right,
+    left,
+    assumption,
+  right,
+  right,
+  assumption
+end
+
+example (p q r : Prop) (hp : p) :
   (p ∨ q ∨ r) ∧ (q ∨ p ∨ r) ∧ (q ∨ r ∨ p) :=
-by simp *
+by repeat { split <|> { try {left}, assumption } <|> right }

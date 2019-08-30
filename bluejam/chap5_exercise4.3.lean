2899c1973513919b7bc3dc5646b2aac4e89d758f
blue-jam <blue-jam@users.noreply.github.com>
2019-08-30 20:28:47 +0900

    Remove "from" from chap5_exercise4.3.lean

--- bluejam/chap5_exercise4.3.lean
+++ bluejam/chap5_exercise4.3.lean
@@ -1,18 +1,21 @@
+    
 variables (men : Type) (barber : men)
 variable  (shaves : men → men → Prop)
 
 example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) :
   false :=
 begin
-  have hb : shaves barber barber ↔ ¬ shaves barber barber, from h barber,
-  have : ¬ shaves barber barber, from (
-    begin
-      apply not.intro,
-      intro,
-      have : ¬ shaves barber barber, from hb.mp a,
-      contradiction
-    end
-  ),
-  have : shaves barber barber, from hb.mpr this,
+  have hb : shaves barber barber ↔ ¬ shaves barber barber,
+    apply h,
+  have : ¬ shaves barber barber,
+    apply not.intro,
+    intro,
+    have : ¬ shaves barber barber,
+      apply hb.mp,
+    assumption,
+    contradiction,
+  have : shaves barber barber,
+    apply hb.mpr,
+    assumption,
   contradiction
 end

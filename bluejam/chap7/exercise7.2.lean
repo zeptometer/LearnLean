90da3854a721e31467f4bef11b30ae28d979f634
blue-jam <blue-jam@users.noreply.github.com>
2019-09-19 19:44:38 +0900

    Update exercise7.2.lean

--- bluejam/chap7/exercise7.2.lean
+++ bluejam/chap7/exercise7.2.lean
@@ -4,6 +4,9 @@ namespace hidden
 
 variables (α β γ : Type u)
 
+example : inhabited bool := inhabited.mk tt
+example : inhabited nat := inhabited.mk 0
+
 def partial_composite : (α → option β) → (β → option γ) → (α → option γ)
 := sorry
 

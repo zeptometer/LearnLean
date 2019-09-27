a9bafebab2c923127b0e2d76ce7b06aeb2fc841b
na4zagin3 <mrty.ityt.pt@gmail.com>
2019-09-27 10:26:31 +0300

    Solve chp 3 redo in chp 5

--- na4zagin3/chp-5.lean
+++ na4zagin3/chp-5.lean
@@ -53,17 +53,84 @@ begin
   cases h; cases h with hq hqr; split;
   { assumption <|> { left; assumption}  <|> { right; assumption} }
 end
-example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) :=
+begin
+  split,
+    intros h,
+    cases h with hp hqr;
+    try {cases hqr with hq hr}; {
+      split; {left; assumption} <|> {right; assumption}
+    },
+  intros h,
+  cases h with hpq hpr;
+  cases hpq with hp hq;
+  cases hpr with hp2 hr;
+  { assumption <|> {left; assumption}  <|> { right; split; assumption} }
+end
 
 -- other properties
-example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
-example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
-example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
-example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
-example : ¬(p ∧ ¬p) := sorry
-example : p ∧ ¬q → ¬(p → q) := sorry
-example : ¬p → (p → q) := sorry
-example : (¬p ∨ q) → (p → q) := sorry
+example : (p → (q → r)) ↔ (p ∧ q → r) :=
+begin
+  split,
+    intros hpqr hpq,
+    cases hpq with hp hq,
+    apply hpqr; assumption,
+  intros hpqr hp hq,
+  apply hpqr; split; assumption,
+end
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) :=
+begin
+  split,
+    intros hpqr,
+    split; intros hpq; apply hpqr; {{left; assumption} <|> {right; assumption}},
+  intros hprqr hpq,
+  cases hprqr with hpr hqr,
+  cases hpq with hp hq; { apply hpr; assumption } <|> { apply hqr; assumption }
+end
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q :=
+begin
+  split,
+    intros hnpq,
+    split; intros hpq; apply hnpq; {left; assumption}  <|> { right; assumption},
+  intros hnpnq hpq,
+  cases hnpnq with hnp hnq,
+  cases hpq with hp hq;
+  { apply hnp; assumption } <|> { apply hnq; assumption }
+end
+example : ¬p ∨ ¬q → ¬(p ∧ q) :=
+begin
+  intros hnpnq hpq,
+  cases hpq with hp hq,
+  cases hnpnq with hnp hnq;
+  { apply hnp; assumption } <|> { apply hnq; assumption }
+end
+example : ¬(p ∧ ¬p) :=
+begin
+  intros hpnp,
+  cases hpnp with hp hnp,
+  apply hnp,
+  assumption,
+end
+example : p ∧ ¬q → ¬(p → q) :=
+begin
+  intros hpnq hpq,
+  cases hpnq with hp hnq,
+  apply hnq,
+  apply hpq,
+  apply hp,
+end
+example : ¬p → (p → q) :=
+begin
+  intros hnp hp,
+  cases (hnp hp),
+end
+example : (¬p ∨ q) → (p → q) :=
+begin
+  intros hnpq hp,
+  cases hnpq with hnp hq,
+    cases (hnp hp),
+  apply hq,
+end
 end sec_3_redo
 
 example (p q r : Prop) (hp : p) :

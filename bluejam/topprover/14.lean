ff4df958d5bcceef271a54e91f11bcb0a3510b36
blue-jam <blue-jam@users.noreply.github.com>
2019-11-07 22:30:12 +0900

    Update 14.lean

--- bluejam/topprover/14.lean
+++ bluejam/topprover/14.lean
@@ -8,7 +8,7 @@ def mod2 : nat → nat
   | (succ n) := 0
   end
 
-lemma h1 : ∀ (n : nat), mod2 (n + 2) = mod2 n :=
+lemma add2 : ∀ (n : nat), mod2 (n + 2) = mod2 n :=
 begin
   intros,
   unfold mod2,
@@ -17,8 +17,32 @@ begin
   simp [mod2, n_ih]
 end
 
-theorem task : ∀ (n : nat), mod2 n = mod2 (n * n) :=
+lemma lem : ∀ (k n: nat), mod2 (n + k + k) = mod2 n :=
 begin
-  intros,
-  
+    intro k, induction k,
+        simp [mod2],
+    intros,
+    show mod2 (n + succ k_n + succ k_n) = mod2 n, from calc
+    mod2 (n + succ k_n + succ k_n)
+        = mod2 (n + (k_n + 1) + (k_n + 1)) : by refl
+    ... = mod2 ((n + k_n + k_n) + 2) : by simp [add_comm, add_assoc]
+    ... = mod2 (n + k_n + k_n) : by rw add2
+    ... = mod2 n : by rw k_ih
+end
+
+example : ∀ (n : nat), mod2 n = mod2 (n * n) :=
+begin
+    intros,
+    induction n,
+        simp,
+    simp [mul_succ, succ_mul],
+    rw (add_comm (succ n_n)),
+    rw ←(add_assoc n_n),
+    rw add_succ,
+    unfold mod2,
+    have: mod2 (n_n + n_n * n_n + n_n) = mod2 n_n, from calc
+    mod2 (n_n + n_n * n_n + n_n) = mod2 (n_n * n_n + n_n + n_n) : by simp [add_assoc, add_comm]
+    ... = mod2 (n_n * n_n) : by rw (lem n_n (n_n * n_n))
+    ... = mod2 n_n : by simp [n_ih],
+    simp only [this],
 end

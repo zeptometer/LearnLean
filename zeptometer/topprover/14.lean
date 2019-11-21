49bc19f87a7b06989407d31f171af1f1edbe61f8
Yuito Murase <ymurase@indeed.com>
2019-11-21 19:46:01 +0900

    Prove even-odd version of topprover 14

--- zeptometer/topprover/14.lean
+++ zeptometer/topprover/14.lean
@@ -9,78 +9,89 @@ def mod2 : ℕ → ℕ
     | (succ n) := 0
     end
 
-mutual inductive even, odd
-with even : ℕ → Prop
+inductive even : ℕ → Prop
 | even_zero : even 0
-| even_succ : ∀ n, odd n → even (n + 1)
-with odd : ℕ → Prop
+| even_succ : ∀ n, even n → even (n + 2)
+
+inductive odd : ℕ → Prop
 | odd_succ : ∀ n, even n → odd (n + 1)
 
-mutual lemma succ_even_odd, succ_odd_even
-with succ_even_odd : ∀n, even (n + 1) → odd n
-| n := begin
-    intro n_succ_even,
-    cases n_succ_even,
-    assumption
-end
-with succ_odd_even : ∀n, odd (n + 1) → even n
-| n := begin
-    intro n_succ_odd,
-    cases n_succ_odd,
+lemma even_or_odd : ∀n, even n ∨ odd n := begin
+    intro n,
+    induction n,
+        left,
+        apply even.even_zero,
+    cases n_ih,
+        right,
+        apply odd.odd_succ,
+        assumption,
+    left,
+    induction n_n,
+    cases n_ih,
+    apply even.even_succ,
+    cases n_ih,
     assumption
 end
 
-mutual lemma even_add_preserves_evenness, odd_add_switches_evenness
-with even_add_preserves_evenness
-    : ∀n, even n → ∀m, even m ↔ even (n + m)
-| n := begin
-    induction n,
-        intro,
-        simp,
-    intro n_n_succ_is_even,
-    simp [add_succ],
+lemma not_even_and_odd : ∀n, even n → odd n → false := begin
+    intros n e_n,
+    induction e_n,
+        intro o_zero,
+        cases o_zero,
+    intro o_e_2,
+    cases o_e_2,
+    cases o_e_2_a,
     
 end
-with odd_add_switches_evenness
-    : ∀n, odd n = tt → ∀m, even m ↔ odd (n + m)
-| _ := begin
-    sorry
-end
 
-lemma even_addition_preserves_evenness :
-    ∀n, even n = tt → ∀m, even m = even (n + m) := begin
-        intro n,
-        induction n,
-        intro,
-        intro m,
-        simp,
-        simp [even, add_succ],   
-        intro n_is_odd,
-        induction n_n,
-        simp [even, odd] at n_is_odd,
-        cases n_is_odd,
-        simp [odd, add_succ],
-        simp [odd] at n_is_odd,
-        revert n_is_odd,
-        simp [even, add_succ] at n_ih,
-        
-    end
-
-lemma pohe : ∀n m, even n = even m → ∀l, even l = even (n + m + l) := begin
-    intros n m,
-    induction n,
-    induction m,
+lemma even_plus_even_is_even : ∀n, even n → ∀m, even m → even (n + m) := begin
+    intros n e_n,
+    induction e_n,
         intros,
         simp,
-    intro h,
-
+        assumption,
+    intros m em,
+    have comm : e_n_n + 2 + m = (e_n_n + m) + 2 := by simp,
+    rw comm,
+    apply even.even_succ,
+    apply e_n_ih,
+    assumption
 end
 
-example : ∀n, even n = even (n * n) := begin
+lemma even_is_two_times : ∀n, even (2 * n) := begin
     intro n,
     induction n,
         simp,
-        rw succ_mul,
-        rw mul_succ,
+        apply even.even_zero,
+    rw mul_succ,
+    have two_is_even : even 2 := even.even_succ 0 even.even_zero,
+    apply even_plus_even_is_even;
+    assumption
+end
 
+lemma even_times_is_even : ∀n, even n → ∀m, even (n * m) := begin
+    intros n e_n,
+    induction e_n,
+        intros,
+        simp,
+        apply even.even_zero,
+    intro m,
+    rw right_distrib,
+    have e_2_m : even (2 * m) := even_is_two_times m,
+    apply even_plus_even_is_even,
+    apply e_n_ih,
+    assumption
+end
+
+example : ∀n, even n ↔ even (n * n) := begin
+    intro n,
+    apply iff.intro,
+        intro e_n,
+        apply even_times_is_even,
+        assumption,
+    have n_e_o : even n ∨ odd n := even_or_odd n,
+    cases n_e_o,
+        intros,
+        assumption,
+    
 end

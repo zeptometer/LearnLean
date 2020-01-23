7b8cc79cfa1a70f9f56f1e4d5c1c4f4f375de6ef
blue-jam <blue-jam@users.noreply.github.com>
2020-01-23 20:14:59 +0900

    Update 06.lean

--- bluejam/topprover/06.lean
+++ bluejam/topprover/06.lean
@@ -1,16 +1,1 @@
-example : forall (n m : nat), n < m \/ n = m \/ n > m :=
-begin
-    intros,
-    have h : n < m ∨ n ≥ m, from lt_or_ge n m,
-    cases h,
-        left,
-        assumption,
-    right,
-    have : m = n ∨ m < n, from nat.eq_or_lt_of_le h,
-    cases this,
-        left,
-        symmetry,
-        assumption,
-    right,
-    apply this,
-end
+example : forall (n m : nat), n < m \/ n = m \/ n > m := nat.lt_trichotomy

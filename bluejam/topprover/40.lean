ec724f02d57b967bc2eae0ad5733fccc9b77447b
blue-jam <blue-jam@users.noreply.github.com>
2020-02-13 20:50:00 +0900

    Create 40.lean

---
+++ bluejam/topprover/40.lean
@@ -0,0 +1,36 @@
+variable f : nat -> bool.
+
+def downward_closed :=
+  forall m n, m <= n -> f n = true -> f m = true.
+
+-- k: an extra parameter to convince termination checker
+def binsearch : nat → nat → nat → nat
+| l r 0 := match decidable.to_bool (nat.succ l = r) with
+    | tt := l
+    | ff := r
+    end
+| l r (k + 1) := match decidable.to_bool (nat.succ l = r) with
+    | tt := l
+    | ff :=
+        let m := (l + r) / 2 in
+        if f m then binsearch m r k else binsearch l m k
+end
+
+lemma success : downward_closed f → ∀ (l r : nat), l < r → f l = tt → f r = ff → f (binsearch f l r (r - l)) = tt :=
+sorry
+
+example: downward_closed f →
+    ∀ (l r : nat), l < r → f l = tt → f r = ff →
+        ∃ (k : nat), ∀ n, f n = tt ↔ n ≤ (binsearch f l r k) :=
+begin
+    intros h l r h_l_lt_r h_low h_up,
+    fapply exists.intro,
+        exact (r - l),
+    intro n,
+    split,
+        intro h2,
+        induction (r - l),
+            sorry,
+        sorry,
+    sorry
+end

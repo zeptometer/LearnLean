13a4c6fb51909ca33ec3ec0b7a5b3df328f9d2d0
blue-jam <blue-jam@users.noreply.github.com>
2020-03-12 20:08:11 +0900

    Update 04.lean

--- bluejam/topprover/04.lean
+++ bluejam/topprover/04.lean
@@ -1,6 +1,28 @@
 lemma at_most_two : ∀ (f : bool → bool) b x y z,
     x = f b → y = f x → z = f y → z = x :=
-    sorry
+begin
+    intros f b x y z h1 h2 h3,
+    induction b,
+        induction x,
+            rw ←h1 at h2,
+            rw h2 at h3,
+            simp [h3],
+            rw ←h1,
+        rw h1,
+        induction y,
+            assumption,
+        simp * at *,
+        rw ←h2,
+    induction x,
+        induction y,
+            rw ←h2 at h3,
+            assumption,
+        rw h3,
+        rw ←h1,
+    rw ←h1 at h2,
+    rw [h2, ←h1] at h3,
+    assumption,
+end
 
 example : ∀ (f : bool → bool) b, f (f (f b)) = f b :=
 by intros; apply at_most_two; refl
